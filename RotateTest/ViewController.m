fa6e72c0a53f1c5c5f9ba0bd00397f7290602930
John Stewart <jstewart@Johns-MacBook-Air.local>
2014-01-10 12:13:01 -0600

    Initial Commit

---
+++ RotateTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RotateTest
+//
+//  Created by John Stewart on 1/10/14.
+//  Copyright (c) 2014 John Stewart. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
