12c0e5f9b239178bad9e5f16a2385be83d875b9b
John Stewart <jstewart@Johns-MacBook-Air.local>
2014-01-10 12:27:12 -0600

    Quick project to test rotation forcing

--- RotateTest/ViewController.m
+++ RotateTest/ViewController.m
@@ -6,19 +6,57 @@
 //  Copyright (c) 2014 John Stewart. All rights reserved.
 //
 
+// A quick project to test suggestions from http://stackoverflow.com/questions/20987249/how-do-i-programmatically-set-device-orientation-in-ios7/
+
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property (nonatomic) bool modePortraitOnly;
 
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self.portraitOnlySwitch addTarget:self action:@selector(controlChanged:) forControlEvents:UIControlEventValueChanged];
+    [self.portraitOnlySwitch setOn:TRUE];
+    self.modePortraitOnly = TRUE;
 }
+
+- (void) controlChanged:(id)sender {
+    self.modePortraitOnly = self.portraitOnlySwitch.isOn;
+
+    if (self.modePortraitOnly) {
+        // We just changed to portrait-only; try to force a rotation into portrait
+        
+        // Charan Giri suggestion
+        [self willRotateToInterfaceOrientation:UIInterfaceOrientationPortrait duration:1.0];
+        
+        // Sunny shah suggestion (works! Hack, unsupported)
+//        objc_msgSend([UIDevice currentDevice], @selector(setOrientation:), UIInterfaceOrientationPortrait );
+
+    }
+}
+
+- (NSUInteger) supportedInterfaceOrientations {
+    if (self.modePortraitOnly) {
+        return UIInterfaceOrientationMaskPortrait | UIInterfaceOrientationMaskPortraitUpsideDown;
+    } else {
+        return UIInterfaceOrientationMaskAll;
+    }
+}
+
+
+- (BOOL) shouldAutorotate {
+    return TRUE;
+}
+
 
 - (void)didReceiveMemoryWarning
 {
